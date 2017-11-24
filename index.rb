0dbbd90142a95c6351c2ca12f358aefbc2599202
tylteng <teng.ty4@gmail.com>
2017-11-24 15:31:33 -0500

    completed exercise

---
+++ index.rb
@@ -0,0 +1,24 @@
+ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
+           {1 => 'Bella', 2 => 'Lucky', 3 => 'Tigger'},
+           {1 => 'Bella', 2 => 'Boots', 3 => 'Smudge'},
+           {1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
+
+tally = {'Smudge' => 0, 'Tigger' => 0, 'Simba' => 0, 'Bella' => 0, 'Lucky' => 0, 'Boots' => 0, 'Felix' => 0}
+
+ballots.each do |x|
+  tally.each do |name, value|
+    if x.has_value?(name)
+      if x.key(name) == 1
+        tally[name] = value + 3
+      elsif x.key(name) == 2
+        tally[name] = value + 2
+      else x.key(name) == 1
+        tally[name] = value + 1
+      end
+    end
+  end
+end
+
+p tally
